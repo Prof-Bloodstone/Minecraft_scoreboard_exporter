2798189ff6b61f9203b151f2608b08e2bab7f9b9
Professor Bloodstone <prof.bloodstone@gmail.com>
2019-10-03 14:19:01 +0200

    Fix CRON integration

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,18 @@ FROM python:3.7-alpine
 
 RUN apk add --update \
     bash \
+    busybox-suid \
     && rm -rf /var/cache/apk/*
 
-COPY requirements.txt /
+ENV CRON_USER=minecraft
+
+RUN addgroup -S $CRON_USER && adduser -S $CRON_USER -G $CRON_USER
+
+COPY --chown=$CRON_USER:$CRON_USER requirements.txt /
 
 RUN pip install -r requirements.txt
 
-COPY mc_NBT_top_scores.py entrypoint.sh /
+COPY --chown=$CRON_USER:$CRON_USER mc_NBT_top_scores.py entrypoint.sh /
 
 RUN chmod +x /mc_NBT_top_scores.py /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,11 +13,14 @@ if [ "${1}" = 'mc_NBT_top_scores.py' ]; then
     set -- mc_NBT_top_scores.py '--config' '/config.json' "${@}"
   fi
   if [ -n "${CRON_SCHEDULE:-}" ]; then
-    printf '%s python3 -u %s\n' "${CRON_SCHEDULE}" "$(printf "'%s' " "${@}")" | crontab -
-    exec crond -f -L /dev/stdout
-  else
-    exec python3 -u "${@}"
+    if [ "$(id -u)" == 0 ]; then
+      printf '%s cd / && python3 -u %s\n' "${CRON_SCHEDULE}" "$(printf "'%s' " "${@}")" | crontab -u "${CRON_USER:-minecraft}" -
+      exec crond -f -L /dev/stdout
+    else
+      printf 'Not running as root when CRON_SCHEDULE is set - will not use cron\n' >&2
+    fi
   fi
+    exec python3 -u "${@}"
 fi
 
 # In case user wants to run something else
